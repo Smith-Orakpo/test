e72ff7e1975b05c7f991ed09168596606229f25e
ogheneovo orakpo <orakpoogheneovo@gmail.com>
2022-03-07 15:12:02 +0100

    scaffold express app

---
+++ index.js
@@ -0,0 +1,7 @@
+const express = require('express')
+
+const app = express()
+
+app.get('/', (req, res) => res.status(200).send('<h1>hello world</h1>'))
+
+app.listen(5000, () => console.log('express running...'))
